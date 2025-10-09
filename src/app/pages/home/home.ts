78c9e9064b48d3c26f607ffe53aed74bf309522c
mateus <mateusfrazao12345@gmail.com>
2025-10-09 16:54:52 -0300

    finalização do carrousel

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+    bannerImgs= [
+       {
+        id: 1,
+        img: '/7dcc28ed89760319.webp',
+       },
+        {
+        id: 2,
+        img: '/9021283f0be266c1.webp',
+       },
+        {
+        id: 3,
+        img: 'ef637eb93bf1a887.webp',
+       }
+    ]
 }
